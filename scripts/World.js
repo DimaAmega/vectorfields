547a37f275f20c2cea54605f948ce79b8d908fc5
DimaAmega <dima.amega1@gmail.com>
2023-01-29 23:06:22 +0400

    prettier

--- scripts/World.js
+++ scripts/World.js
@@ -1,256 +1,287 @@
-PIXI.utils.skipHello();
+PIXI.utils.skipHello()
 function World() {
   //////////////////
   //    PRIVATE
   //////////////////
-  const coord_Lines = new PIXI.Graphics();
-  const divStage = document.getElementById("stage");
-  let start_p = undefined;
-  let cw = divStage.clientWidth, ch = divStage.clientHeight, scale = 60, x_off = 0, y_off = 0, o_x_off = 0, o_y_off = 0;
-  let arr_particles = [], M_Time_Alive_particle = 1.5, h_integrate = 1e-3, M_n_lines = 20, xspeed = 1, defSpeed = 10;
-  let Pole = (_) => { return [0, 0] };
+  const coord_Lines = new PIXI.Graphics()
+  const divStage = document.getElementById('stage')
+  let start_p = undefined
+  let cw = divStage.clientWidth,
+    ch = divStage.clientHeight,
+    scale = 60,
+    x_off = 0,
+    y_off = 0,
+    o_x_off = 0,
+    o_y_off = 0
+  let arr_particles = [],
+    M_Time_Alive_particle = 1.5,
+    h_integrate = 1e-3,
+    M_n_lines = 20,
+    xspeed = 1,
+    defSpeed = 10
+  let Pole = _ => {
+    return [0, 0]
+  }
   const app = new PIXI.Application({
     antialias: true,
-    resizeTo: document.getElementById("stage")
-  }
-  );
-  app.stage.addChild(coord_Lines);
+    resizeTo: document.getElementById('stage'),
+  })
+  app.stage.addChild(coord_Lines)
 
   const getRandomArbitary = (min, max) => {
-    return Math.random() * (max - min) + min;
-  };
+    return Math.random() * (max - min) + min
+  }
   const gxLeft = () => {
-    return - (cw / 2 + x_off) / scale;
-  };
+    return -(cw / 2 + x_off) / scale
+  }
   const gxRight = () => {
-    return (cw / 2 - x_off) / scale;
-  };
+    return (cw / 2 - x_off) / scale
+  }
   const gyTop = () => {
-    return (ch / 2 + y_off) / scale;
-  };
+    return (ch / 2 + y_off) / scale
+  }
   const gyBottom = () => {
-    return - (ch / 2 - y_off) / scale;
-  };
+    return -(ch / 2 - y_off) / scale
+  }
   const arrayData = (data, n) => {
-    const res = [];
-    for (let i = 0; i < n; i++) res.push([data[0], data[1]]);
-    return res;
+    const res = []
+    for (let i = 0; i < n; i++) res.push([data[0], data[1]])
+    return res
   }
   const getRandomCoords = () => {
-    return [getRandomArbitary(gxLeft(), gxRight()), getRandomArbitary(gyBottom(), gyTop())];
-  };
-  const gxc = (x) => x * scale + cw / 2 + x_off;
-  const gyc = (y) => ch / 2 - y * scale + y_off;
+    return [
+      getRandomArbitary(gxLeft(), gxRight()),
+      getRandomArbitary(gyBottom(), gyTop()),
+    ]
+  }
+  const gxc = x => x * scale + cw / 2 + x_off
+  const gyc = y => ch / 2 - y * scale + y_off
   const addOnWheel = (elem, handler) => {
     if (elem.addEventListener) {
       if ('onwheel' in document) {
         // IE9+, FF17+
-        elem.addEventListener("wheel", handler);
+        elem.addEventListener('wheel', handler)
       } else if ('onmousewheel' in document) {
-        elem.addEventListener("mousewheel", handler);
+        elem.addEventListener('mousewheel', handler)
       } else {
-        elem.addEventListener("MozMousePixelScroll", handler);
+        elem.addEventListener('MozMousePixelScroll', handler)
       }
-    } else { // IE8-
-      text.attachEvent("onmousewheel", handler);
+    } else {
+      // IE8-
+      text.attachEvent('onmousewheel', handler)
     }
-  };
-  const getLengthOfVec = (vec) => {
-    return Math.pow(vec[0] ** 2 + vec[1] ** 2, 0.5);
-  };
+  }
+  const getLengthOfVec = vec => {
+    return Math.pow(vec[0] ** 2 + vec[1] ** 2, 0.5)
+  }
 
-  const getColorPole = (vec) => {
-    const length = getLengthOfVec(vec);
-    const LR = 150;
-    let component = Math.floor(LR * length);
-    if (component > 255) component = 255;
-    return (255 - component << 16) | component;
-  };
+  const getColorPole = vec => {
+    const length = getLengthOfVec(vec)
+    const LR = 150
+    let component = Math.floor(LR * length)
+    if (component > 255) component = 255
+    return ((255 - component) << 16) | component
+  }
 
-  const mousemove = (e) => {
-    const dx = e.clientX - start_p.x;
-    const dy = e.clientY - start_p.y;
-    x_off = o_x_off + dx;
-    y_off = o_y_off + dy;
-    drawPlan();
-  };
-  const fingerMouse = (e) => {
+  const mousemove = e => {
+    const dx = e.clientX - start_p.x
+    const dy = e.clientY - start_p.y
+    x_off = o_x_off + dx
+    y_off = o_y_off + dy
+    drawPlan()
+  }
+  const fingerMouse = e => {
     if (e.changedTouches.length == 1) {
-      const dx = e.changedTouches[0].clientX - start_p.x;
-      const dy = e.changedTouches[0].clientY - start_p.y;
-      x_off = o_x_off + dx;
-      y_off = o_y_off + dy;
-      drawPlan();
+      const dx = e.changedTouches[0].clientX - start_p.x
+      const dy = e.changedTouches[0].clientY - start_p.y
+      x_off = o_x_off + dx
+      y_off = o_y_off + dy
+      drawPlan()
     }
-  };
+  }
   const drawPlan = () => {
-    const iterxL = Math.floor((cw / 2 + x_off) / scale);
-    const iterxR = Math.floor((cw / 2 - x_off) / scale);
-    const iteryT = Math.floor((ch / 2 + y_off) / scale);
-    const iteryB = Math.floor((ch / 2 - y_off) / scale);
-    coord_Lines.clear();
-    coord_Lines.lineStyle(3, 0xffffff, .6);
-    if (((ch / 2 + y_off) > 0) && ((ch / 2 + y_off) < ch)) {
-      coord_Lines.moveTo(0, ch / 2 + y_off).lineTo(cw, (ch / 2) + y_off);
+    const iterxL = Math.floor((cw / 2 + x_off) / scale)
+    const iterxR = Math.floor((cw / 2 - x_off) / scale)
+    const iteryT = Math.floor((ch / 2 + y_off) / scale)
+    const iteryB = Math.floor((ch / 2 - y_off) / scale)
+    coord_Lines.clear()
+    coord_Lines.lineStyle(3, 0xffffff, 0.6)
+    if (ch / 2 + y_off > 0 && ch / 2 + y_off < ch) {
+      coord_Lines.moveTo(0, ch / 2 + y_off).lineTo(cw, ch / 2 + y_off)
     }
-    if (((cw / 2 + x_off) > 0) && ((cw / 2 + x_off) < cw)) {
-      coord_Lines.moveTo(cw / 2 + x_off, 0).lineTo(cw / 2 + x_off, ch);
+    if (cw / 2 + x_off > 0 && cw / 2 + x_off < cw) {
+      coord_Lines.moveTo(cw / 2 + x_off, 0).lineTo(cw / 2 + x_off, ch)
     }
-    coord_Lines.lineStyle(1, 0xffffff, .212);
-
+    coord_Lines.lineStyle(1, 0xffffff, 0.212)
 
     for (let i = -iterxL; i <= iterxR; i++)
-      coord_Lines.moveTo(x_off + ((cw) / 2) + i * scale, 0).lineTo(x_off + ((cw) / 2) + i * scale, ch);
+      coord_Lines
+        .moveTo(x_off + cw / 2 + i * scale, 0)
+        .lineTo(x_off + cw / 2 + i * scale, ch)
     for (let i = -iteryT; i <= iteryB; i++)
-      coord_Lines.moveTo(0, y_off + ch / 2 + i * scale).lineTo(cw, y_off + ch / 2 + i * scale);
-  };
+      coord_Lines
+        .moveTo(0, y_off + ch / 2 + i * scale)
+        .lineTo(cw, y_off + ch / 2 + i * scale)
+  }
   const addParticle = () => {
-    const lines_count = Math.floor(getRandomArbitary(2, 2 * M_n_lines - 2));
+    const lines_count = Math.floor(getRandomArbitary(2, 2 * M_n_lines - 2))
     const particle = {
       self: new PIXI.Graphics(),
       data: arrayData(getRandomCoords(), lines_count),
-      time_live: Math.floor(getRandomArbitary(lines_count, M_Time_Alive_particle * lines_count)),
+      time_live: Math.floor(
+        getRandomArbitary(lines_count, M_Time_Alive_particle * lines_count),
+      ),
       c_time: 0,
-    };
-    app.stage.addChild(particle.self);
-    arr_particles.push(particle);
-  };
+    }
+    app.stage.addChild(particle.self)
+    arr_particles.push(particle)
+  }
   const dropParticle = () => {
-    arr_particles.pop().self.clear();
-  };
+    arr_particles.pop().self.clear()
+  }
   //////////////////
   //    PUBLIC
   //////////////////
 
-  this.ticker = app.ticker;
+  this.ticker = app.ticker
   this.initializate = () => {
-    divStage.appendChild(app.view);
-    window.addEventListener("resize", () => {
-      cw = divStage.clientWidth;
-      ch = divStage.clientHeight;
-      drawPlan();
-    });
-    app.view.addEventListener("mousedown", (e) => {
-      start_p = { x: e.clientX, y: e.clientY };
-      o_x_off = x_off;
-      o_y_off = y_off;
-      app.view.addEventListener("mousemove", mousemove);
-    });
-    app.view.addEventListener("mouseup", (e) => {
-      app.view.removeEventListener("mousemove", mousemove);
-    });
-    app.view.addEventListener("touchstart", (e) => {
-      console.log(e);
-      start_p = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
-      o_x_off = x_off;
-      o_y_off = y_off;
-      app.view.addEventListener("touchmove", fingerMouse);
-    });
-    app.view.addEventListener("touchend", (e) => {
-      console.log(e);
-      app.view.removeEventListener("touchmove", fingerMouse);
-    });
-    addOnWheel(app.view, (e) => {
-      const delta = e.deltaY || e.detail || e.wheelDelta;
+    divStage.appendChild(app.view)
+    window.addEventListener('resize', () => {
+      cw = divStage.clientWidth
+      ch = divStage.clientHeight
+      drawPlan()
+    })
+    app.view.addEventListener('mousedown', e => {
+      start_p = { x: e.clientX, y: e.clientY }
+      o_x_off = x_off
+      o_y_off = y_off
+      app.view.addEventListener('mousemove', mousemove)
+    })
+    app.view.addEventListener('mouseup', e => {
+      app.view.removeEventListener('mousemove', mousemove)
+    })
+    app.view.addEventListener('touchstart', e => {
+      console.log(e)
+      start_p = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY,
+      }
+      o_x_off = x_off
+      o_y_off = y_off
+      app.view.addEventListener('touchmove', fingerMouse)
+    })
+    app.view.addEventListener('touchend', e => {
+      console.log(e)
+      app.view.removeEventListener('touchmove', fingerMouse)
+    })
+    addOnWheel(app.view, e => {
+      const delta = e.deltaY || e.detail || e.wheelDelta
 
-      const X = (e.clientX - cw / 2 - x_off) / scale;
-      const Y = (ch / 2 + y_off - e.clientY) / scale;
+      const X = (e.clientX - cw / 2 - x_off) / scale
+      const Y = (ch / 2 + y_off - e.clientY) / scale
 
-      if ((scale <= 30) && (delta > 0)) scale = 30; else
-        if ((scale >= 300) && (delta < 0)) scale = 300; else
-          if (delta > 0) scale -= 2;
-          else scale += 2;
+      if (scale <= 30 && delta > 0) scale = 30
+      else if (scale >= 300 && delta < 0) scale = 300
+      else if (delta > 0) scale -= 2
+      else scale += 2
 
       if (delta > 0) {
-        x_off += 2 * X;
-        y_off -= 2 * Y;
+        x_off += 2 * X
+        y_off -= 2 * Y
       } else {
-        x_off -= 2 * X;
-        y_off += 2 * Y;
+        x_off -= 2 * X
+        y_off += 2 * Y
       }
-      drawPlan();
-      e.preventDefault();
-    });
-    drawPlan();
-    return this;
-  };
-  this.ADDParticle = (number) => {
-    for (let i = 0; i < number; i++) addParticle();
-  };
-  this.DROPParticle = (number) => {
-    for (let i = 0; i < number; i++) dropParticle();
-  };
+      drawPlan()
+      e.preventDefault()
+    })
+    drawPlan()
+    return this
+  }
+  this.ADDParticle = number => {
+    for (let i = 0; i < number; i++) addParticle()
+  }
+  this.DROPParticle = number => {
+    for (let i = 0; i < number; i++) dropParticle()
+  }
   this.getLength = () => {
-    return arr_particles.length;
-  };
-  this.change_alive_particle = (value) => {
-    M_Time_Alive_particle = value;
-  };
-  this.change_h_integrate = (value) => {
-    h_integrate = value;
-  };
-  this.change_x_speed = (value) => {
-    xspeed = value;
-  };
-  this.change_M_n_lines = (value) => {
-    M_n_lines = value;
-  };
-  this.SetPole = (pole) => {
-    Pole = pole;
-  };
+    return arr_particles.length
+  }
+  this.change_alive_particle = value => {
+    M_Time_Alive_particle = value
+  }
+  this.change_h_integrate = value => {
+    h_integrate = value
+  }
+  this.change_x_speed = value => {
+    xspeed = value
+  }
+  this.change_M_n_lines = value => {
+    M_n_lines = value
+  }
+  this.SetPole = pole => {
+    Pole = pole
+  }
   this.getCoordinates = (w, h) => {
-    return { x: ((w - x_off - cw / 2) / scale).toFixed(2), y: ((y_off - h + ch / 2) / scale).toFixed(2) };
-  };
-  this.ADDScale = (n) => {
-    const X = -(x_off) / scale;
-    const Y = (y_off) / scale;
+    return {
+      x: ((w - x_off - cw / 2) / scale).toFixed(2),
+      y: ((y_off - h + ch / 2) / scale).toFixed(2),
+    }
+  }
+  this.ADDScale = n => {
+    const X = -x_off / scale
+    const Y = y_off / scale
 
-    if ((scale <= 30) && (n < 0)) scale = 30; else
-      if ((scale >= 300) && (n > 0)) scale = 300;
-      else scale += n;
+    if (scale <= 30 && n < 0) scale = 30
+    else if (scale >= 300 && n > 0) scale = 300
+    else scale += n
 
-    x_off -= n * X;
-    y_off += n * Y;
+    x_off -= n * X
+    y_off += n * Y
 
-    drawPlan();
-  };
+    drawPlan()
+  }
   this.updateParticles = () => {
-    const arr = arr_particles;
-    const n = arr.length;
+    const arr = arr_particles
+    const n = arr.length
     for (let i = 0; i < n; i++) {
-      const part = arr[i];
+      const part = arr[i]
       if (part.c_time == part.time_live) {
         if (part.data.length == 1) {
           // UPDATE
-          const lines_count = Math.floor(getRandomArbitary(2, 2 * M_n_lines - 2));
-          part.time_live = Math.floor(getRandomArbitary(lines_count, M_Time_Alive_particle * lines_count))
-          part.c_time = 0;
-          part.data = arrayData(getRandomCoords(), lines_count);
+          const lines_count = Math.floor(
+            getRandomArbitary(2, 2 * M_n_lines - 2),
+          )
+          part.time_live = Math.floor(
+            getRandomArbitary(lines_count, M_Time_Alive_particle * lines_count),
+          )
+          part.c_time = 0
+          part.data = arrayData(getRandomCoords(), lines_count)
+        } else {
+          part.data.length -= 1
         }
-        else {
-          part.data.length -= 1;
-        }
+      } else {
+        let new_Vec = part.data[0]
+        let iterations = xspeed * defSpeed + 1
+        while (--iterations > 0) new_Vec = RungKut(Pole, new_Vec, h_integrate)
+        let j = part.data.length - 1
+        while (j > 0) part.data[j] = part.data[--j]
+        part.data[0] = new_Vec
+        part.c_time += 1
       }
-      else {
-        let new_Vec = part.data[0];
-        let iterations = xspeed * defSpeed + 1;
-        while (--iterations > 0) new_Vec = RungKut(Pole, new_Vec, h_integrate);
-        let j = part.data.length - 1;
-        while (j > 0) part.data[j] = part.data[--j];
-        part.data[0] = new_Vec;
-        part.c_time += 1;
-      }
-      part.self.clear();
-      part.self.lineStyle(1, getColorPole(Pole(part.data[0])));
-      part.self.moveTo(gxc(part.data[0][0]), gyc(part.data[0][1]));
-      const N = part.data.length;
-      for (let j = 1; j < N; j++) part.self.lineTo(gxc(part.data[j][0]), gyc(part.data[j][1]));
-    };
-  };
+      part.self.clear()
+      part.self.lineStyle(1, getColorPole(Pole(part.data[0])))
+      part.self.moveTo(gxc(part.data[0][0]), gyc(part.data[0][1]))
+      const N = part.data.length
+      for (let j = 1; j < N; j++)
+        part.self.lineTo(gxc(part.data[j][0]), gyc(part.data[j][1]))
+    }
+  }
   this.toMouseCoord = (x_c, y_c) => {
-    const trace = arr_particles[Math.floor(getRandomArbitary(0, arr_particles.length))];
-    let j = trace.data.length;
-    while (j >= 0) trace.data[--j] = [Number(x_c), Number(y_c)];
-    trace.c_time = 0;
-  };
-};
+    const trace =
+      arr_particles[Math.floor(getRandomArbitary(0, arr_particles.length))]
+    let j = trace.data.length
+    while (j >= 0) trace.data[--j] = [Number(x_c), Number(y_c)]
+    trace.c_time = 0
+  }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
-
 ///////////////////////////
 //    ADDITIONAL FUNC
 ///////////////////////////
 function shareData() {
   const baseData = {
-    x_str: document.getElementById("dx").value,
-    y_str: document.getElementById("dy").value,
-    xspeed: document.getElementById("xspeed").value,
-    count: document.getElementById("count").value,
-    M_Time_Alive_particle: document.getElementById("M_Time_Alive_particle").value,
-    M_n_lines: document.getElementById("M_n_lines").value,
+    x_str: document.getElementById('dx').value,
+    y_str: document.getElementById('dy').value,
+    xspeed: document.getElementById('xspeed').value,
+    count: document.getElementById('count').value,
+    M_Time_Alive_particle: document.getElementById('M_Time_Alive_particle')
+      .value,
+    M_n_lines: document.getElementById('M_n_lines').value,
     // h_integrate: document.getElementById("h_integrate").value
-  };
+  }
   return { ...baseData, ...Parser.Variables }
-};
+}
 
 function createDialog(message) {
-  const div = document.createElement("div");
-  div.setAttribute("id", "messageContainer");
-  const div2 = document.createElement("div");
-  div2.setAttribute("id", "welcome_message");
-  div2.setAttribute("class", "middle");
-  div2.innerHTML = message;
-  div.append(div2);
-  document.body.prepend(div);
-  return div;
-};
+  const div = document.createElement('div')
+  div.setAttribute('id', 'messageContainer')
+  const div2 = document.createElement('div')
+  div2.setAttribute('id', 'welcome_message')
+  div2.setAttribute('class', 'middle')
+  div2.innerHTML = message
+  div.append(div2)
+  document.body.prepend(div)
+  return div
+}
 
 function createHelpDialog() {
-  const startMessage = createDialog(START_MESSAGE);
-  startMessage.addEventListener("click", () => {
-    startMessage.firstChild.setAttribute("class", "tophide");
+  const startMessage = createDialog(START_MESSAGE)
+  startMessage.addEventListener('click', () => {
+    startMessage.firstChild.setAttribute('class', 'tophide')
     setTimeout(() => startMessage.remove(), 600)
-  });
+  })
 }
 
 ///////////////////////////
@@ -58,122 +58,119 @@ const START_MESSAGE = `<h1>Visualization of vector fields online</h1>
   <li>With the Alt/Option key held down, you can see the coordinates in the phase space</li>
   <li>With the Ctrl/Command key held down, you can click on specific point on the phase space to set initial conditions you want integrate from</li>
   </ol>
-</p>`;
+</p>`
 
 ///////////////////////////
 //     INITIALIZATING
 ///////////////////////////
-const world = new World().initializate();
-world.ticker.add(() =>  world.updateParticles());
-const MY_DOMEN = `${location.origin}${location.pathname}`;
+const world = new World().initializate()
+world.ticker.add(() => world.updateParticles())
+const MY_DOMEN = `${location.origin}${location.pathname}`
 
 ///////////////////////////
 //  ADD EVENTS LISTENERS
 ///////////////////////////
-document.getElementById("count").addEventListener("change", (e) => {
-  if (world.getLength() < Number(e.target.value)) world.ADDParticle(Number(e.target.value) - world.getLength());
-  else world.DROPParticle(world.getLength() - Number(e.target.value));
-});
-document.getElementById("M_Time_Alive_particle").addEventListener("change", (e) => {
-  world.change_alive_particle(Number(e.target.value));
-});
-document.getElementById("h_integrate").addEventListener("change", (e) => {
-  world.change_h_integrate(Number(e.target.value));
-});
-document.getElementById("xspeed").addEventListener("change", (e) => {
-  world.change_x_speed(Number(e.target.value));
-});
-document.getElementById("M_n_lines").addEventListener("change", (e) => {
-  world.change_M_n_lines(Number(e.target.value));
-});
-document.getElementById("set").addEventListener("click", (e) => {
-  const x_str = document.getElementById("dx").value;
-  const y_str = document.getElementById("dy").value;
+document.getElementById('count').addEventListener('change', e => {
+  if (world.getLength() < Number(e.target.value))
+    world.ADDParticle(Number(e.target.value) - world.getLength())
+  else world.DROPParticle(world.getLength() - Number(e.target.value))
+})
+document
+  .getElementById('M_Time_Alive_particle')
+  .addEventListener('change', e => {
+    world.change_alive_particle(Number(e.target.value))
+  })
+document.getElementById('h_integrate').addEventListener('change', e => {
+  world.change_h_integrate(Number(e.target.value))
+})
+document.getElementById('xspeed').addEventListener('change', e => {
+  world.change_x_speed(Number(e.target.value))
+})
+document.getElementById('M_n_lines').addEventListener('change', e => {
+  world.change_M_n_lines(Number(e.target.value))
+})
+document.getElementById('set').addEventListener('click', e => {
+  const x_str = document.getElementById('dx').value
+  const y_str = document.getElementById('dy').value
   if (Parser.parse(x_str, y_str)) {
-    world.SetPole(Parser.Pole);
-    createInputs({});
+    world.SetPole(Parser.Pole)
+    createInputs({})
   }
-});
-document.getElementById("scale_p").addEventListener("click", (e) => {
-  world.ADDScale(10);
-});
-document.getElementById("scale_s").addEventListener("click", (e) => {
-  world.ADDScale(-10);
-});
+})
+document.getElementById('scale_p').addEventListener('click', e => {
+  world.ADDScale(10)
+})
+document.getElementById('scale_s').addEventListener('click', e => {
+  world.ADDScale(-10)
+})
 
-document.getElementById("help").addEventListener("click", (e) => {
+document.getElementById('help').addEventListener('click', e => {
   createHelpDialog()
 })
 
-document.getElementById("share").addEventListener("click", (e) => {
-  const dataKeys = shareData();
-  let url = `${MY_DOMEN}?` + "";
-  for (let nameKey in dataKeys) url += `${nameKey}=${dataKeys[nameKey]}&`;
-  const linkDialog = createDialog(`<h3>Ссылка</h3> <p class="message" >${url}</p>`);
-  const button = document.createElement("button");
-  button.innerHTML = "CLOSE";
-  button.addEventListener("click", (e) => {
-    linkDialog.firstChild.setAttribute("class", "tophide");
+document.getElementById('share').addEventListener('click', e => {
+  const dataKeys = shareData()
+  let url = `${MY_DOMEN}?` + ''
+  for (let nameKey in dataKeys) url += `${nameKey}=${dataKeys[nameKey]}&`
+  const linkDialog = createDialog(
+    `<h3>Ссылка</h3> <p class="message" >${url}</p>`,
+  )
+  const button = document.createElement('button')
+  button.innerHTML = 'CLOSE'
+  button.addEventListener('click', e => {
+    linkDialog.firstChild.setAttribute('class', 'tophide')
     setTimeout(() => {
-      linkDialog.remove();
-    }, 600);
-  });
-  linkDialog.firstChild.append(button);
-});
-document.getElementById("hide").addEventListener("click", (e) => {
-  const type = e.target.getAttribute("data");
-  if (type == "on") {
-    e.target.setAttribute("data", "off");
-    e.target.style.bottom = "-30px";
-    document.getElementById("Form").style.right = "-150px";
-    e.target.innerHTML = "Show pannel";
+      linkDialog.remove()
+    }, 600)
+  })
+  linkDialog.firstChild.append(button)
+})
+document.getElementById('hide').addEventListener('click', e => {
+  const type = e.target.getAttribute('data')
+  if (type == 'on') {
+    e.target.setAttribute('data', 'off')
+    e.target.style.bottom = '-30px'
+    document.getElementById('Form').style.right = '-150px'
+    e.target.innerHTML = 'Show pannel'
+  } else {
+    e.target.setAttribute('data', 'on')
+    document.getElementById('Form').style.right = '0'
+    e.target.innerHTML = 'Hide pannel'
   }
-  else {
-    e.target.setAttribute("data", "on");
-    document.getElementById("Form").style.right = "0";
-    e.target.innerHTML = "Hide pannel";
-  }
-});
-document.getElementById('hovered').addEventListener("mouseover", () => {
-  document.getElementById("hide").style.bottom = "0";
-});
-const coords = document.getElementById("coords");
-document.getElementsByTagName("canvas")[0].addEventListener("mousemove", (e) => {
+})
+document.getElementById('hovered').addEventListener('mouseover', () => {
+  document.getElementById('hide').style.bottom = '0'
+})
+const coords = document.getElementById('coords')
+document.getElementsByTagName('canvas')[0].addEventListener('mousemove', e => {
   if (e.altKey) {
-    coords.style.top = `${e.clientY - 60}px`;
-    coords.style.left = `${e.clientX + 10}px`;
+    coords.style.top = `${e.clientY - 60}px`
+    coords.style.left = `${e.clientX + 10}px`
     const c = world.getCoordinates(e.clientX, e.clientY)
-    coords.innerHTML = `x: ${c.x}<br>y: ${c.y}`;
+    coords.innerHTML = `x: ${c.x}<br>y: ${c.y}`
+  } else {
+    coords.style.top = `-100px`
+    coords.style.left = `-100px`
   }
-  else {
-    coords.style.top = `-100px`;
-    coords.style.left = `-100px`;
-  }
-});
-document.getElementsByTagName("canvas")[0].addEventListener("click", (e) => {
+})
+document.getElementsByTagName('canvas')[0].addEventListener('click', e => {
   if (e.ctrlKey || e.metaKey) {
     const coords = world.getCoordinates(e.clientX, e.clientY)
-    world.toMouseCoord(coords.x, coords.y);
+    world.toMouseCoord(coords.x, coords.y)
   }
-});
-
+})
 
 ///////////////////////////
 //   SHARE PROPERTIES
 ///////////////////////////
-const params = window
-  .location
-  .search
+const params = window.location.search
   .replace('?', '')
   .split('&')
-  .reduce(
-    function (p, e) {
-      const a = e.split('=');
-      p[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
-      return p;
-    },
-    {}
-  );
+  .reduce(function (p, e) {
+    const a = e.split('=')
+    p[decodeURIComponent(a[0])] = decodeURIComponent(a[1])
+    return p
+  }, {})
 
 if (params.add_scale) {
   const addScale = Number(params.add_scale)
@@ -185,47 +182,45 @@ if (!params.skip_welcome) {
 }
 
 if (params.x_str && params.y_str) {
-  document.getElementById("dx").value = params.x_str
-  document.getElementById("dy").value = params.y_str
+  document.getElementById('dx').value = params.x_str
+  document.getElementById('dy').value = params.y_str
   if (Parser.parse(params.x_str, params.y_str)) {
-    world.SetPole(Parser.Pole);
-    for (const i in Parser.Variables) Parser.Variables[i] = Number(params[i]) || 0;
-    createInputs(params);
+    world.SetPole(Parser.Pole)
+    for (const i in Parser.Variables)
+      Parser.Variables[i] = Number(params[i]) || 0
+    createInputs(params)
   }
 }
 
 if (params.xspeed) {
-  document.getElementById("xspeed").value = params.xspeed;
-  world.change_x_speed(Number(params.xspeed));
+  document.getElementById('xspeed').value = params.xspeed
+  world.change_x_speed(Number(params.xspeed))
 }
 if (params.count) {
-  document.getElementById("count").value = params.count;
-  world.ADDParticle(Number(params.count));
-}
-else {
-  world.ADDParticle(1000);
+  document.getElementById('count').value = params.count
+  world.ADDParticle(Number(params.count))
+} else {
+  world.ADDParticle(1000)
 }
 
 if (params.M_Time_Alive_particle) {
-  document.getElementById("M_Time_Alive_particle").value = params.M_Time_Alive_particle;
-  world.change_alive_particle(Number(params.M_Time_Alive_particle));
-}
-else {
-  world.change_alive_particle(1.5);
+  document.getElementById('M_Time_Alive_particle').value =
+    params.M_Time_Alive_particle
+  world.change_alive_particle(Number(params.M_Time_Alive_particle))
+} else {
+  world.change_alive_particle(1.5)
 }
 
 if (params.h_integrate) {
-  document.getElementById("h_integrate").value = params.h_integrate;
-  world.change_h_integrate(Number(params.h_integrate));
-}
-else {
-  world.change_h_integrate(1e-3);
+  document.getElementById('h_integrate').value = params.h_integrate
+  world.change_h_integrate(Number(params.h_integrate))
+} else {
+  world.change_h_integrate(1e-3)
 }
 
 if (params.M_n_lines) {
-  document.getElementById("M_n_lines").value = params.M_n_lines;
-  world.change_M_n_lines(Number(params.M_n_lines));
-}
-else {
-  world.change_M_n_lines(20);
+  document.getElementById('M_n_lines').value = params.M_n_lines
+  world.change_M_n_lines(Number(params.M_n_lines))
+} else {
+  world.change_M_n_lines(20)
 }
